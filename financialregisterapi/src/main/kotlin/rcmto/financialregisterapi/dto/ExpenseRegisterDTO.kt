e51635b87d942aa380638b7451f5281331672d9a
rick-cm <rick_mmartins@hotmail.com>
2022-05-07 21:34:41 -0300

    criado Dto de cadastro de expense

---
+++ financialregisterapi/src/main/kotlin/rcmto/financialregisterapi/dto/ExpenseRegisterDTO.kt
@@ -0,0 +1,28 @@
+package rcmto.financialregisterapi.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import rcmto.financialregisterapi.enums.ExpenseStatus
+import rcmto.financialregisterapi.enums.ExpenseType
+import rcmto.financialregisterapi.validator.YearMonthFormat
+import java.math.BigDecimal
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+class ExpenseRegisterDTO(
+    @field:NotNull
+    @field:Positive
+    private var amount:  BigDecimal,
+    @field:NotNull
+    private val description: String,
+    @JsonProperty("reference_date")
+    @YearMonthFormat
+    @field:NotNull
+    private val referenceDate:  String,
+    @field:NotNull
+    private var recurrent: Boolean,
+    @field:NotNull
+    private val type: ExpenseType,
+    @field:NotNull
+    private val status: ExpenseStatus,
+) {
+}
